2154269c82ee55e6915353caee579404c86bf8a8
Alex Noble <alexander.noble@neuraflash.com>
2024-11-28 06:23:22 -0800

    objectType definition update 2

--- src/app/api/salesforce/[objectType]/[id]/route.ts
+++ src/app/api/salesforce/[objectType]/[id]/route.ts
@@ -1,6 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET(request: NextRequest, { params }: { params: { objectType: string; id: string } }) {
+export async function GET(
+    request: NextRequest,
+    { params }: { params: { objectType: string; id: string } }
+) {
     const objectType = await params.objectType
     const id = await params.id
 
