2606107812e7949bf35c70e0a2ff9c76a767de9b
Alex Noble <alexander.noble@neuraflash.com>
2025-02-23 06:39:23 -0800

    removing previous suspense lint override

--- next.config.js
+++ next.config.js
@@ -11,9 +11,6 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
-    experimental: {
-        missingSuspenseWithCSRBailout: false,
-    },
 }
 
 module.exports = nextConfig
